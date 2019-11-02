b1e0a7ed9b831971350481ec0ecb790a5316405f
Hiroshi Miura <miurahr@linux.com>
2019-11-02 12:19:51 +0900

    Introduce read_headers func
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- client.py
+++ client.py
@@ -8,43 +8,41 @@ class AsyncRTSPClient:
         self.reader = None
         self.writer = None
 
-    async def connect(self):
-        print('start connection')
-        self.reader, self.writer = await asyncio.open_connection(self.server, self.port)
-        print('connected wait message...')
+    async def read_headers(self):
         bytes = await self.reader.readline()
         line = bytes.decode('UTF-8')
-        print(":".join("{:02x}".format(ord(c)) for c in line))
+        # print(":".join("{:02x}".format(ord(c)) for c in line))
         headers = []
         while line != '\r\n':
             headers.append(line.rsplit('\r\n')[0])
             bytes = await self.reader.readline()
             line = bytes.decode('UTF-8')
         print(headers)
+        return headers
+
+    async def connect(self):
+        print('start connection')
+        self.reader, self.writer = await asyncio.open_connection(self.server, self.port)
+
+        # message #1 server -> client
+        headers = await self.read_headers()
         if not 'OPTIONS * RTSP/1.0' in headers:
             return False
-
         m1_resp = b'RTSP/1.0 200 OK\r\nCSeq: 1\r\nPublic: org.wfa.wfd1.0, GET_PARAMETER, SET_PARAMETER\r\n\r\n'
         self.writer.write(m1_resp)
         print(m1_resp)
         await self.writer.drain()
 
+        # message #2 client -> sever
         m2_req = b'OPTIONS * RTSP/1.0\r\nCSeq: 100\r\nRequire: org.wfa.wfd1.0\r\n\r\n'
         self.writer.write(m2_req)
         print(m2_req)
         await self.writer.drain()
-
-        bytes = await self.reader.readline()
-        line = bytes.decode('UTF-8')
-        headers = []
-        while line != '\r\n':
-            headers.append(line.rsplit('\r\n')[0])
-            bytes = await self.reader.readline()
-            line = bytes.decode('UTF-8')
-        print(headers)
+        headers = await self.read_headers()
         if not 'RTSP/1.0 200 OK' in headers:
             return False
 
+        # finished
         return True
 
     async def finish(self):
--- server.py
+++ server.py
@@ -3,31 +3,29 @@
 import asyncio
 
 
+async def read_headers(reader):
+    bytes = await reader.readline()
+    line = bytes.decode('UTF-8')
+    headers = []
+    while line != '\r\n':
+        headers.append(line.rsplit('\r\n')[0])
+        bytes = await reader.readline()
+        line = bytes.decode('UTF-8')
+    print(headers)
+    return headers
+
+
 async def connected(reader, writer):
     # m1
     m1_req = b'OPTIONS * RTSP/1.0\r\n\r\n'
     writer.write(m1_req)
     print(m1_req)
     await writer.drain()
-    headers = []
-    bytes = await reader.readline()
-    line = bytes.decode('UTF-8')
-    while line != '\r\n':
-        headers.append(line.rsplit('\r\n')[0])
-        bytes = await reader.readline()
-        line = bytes.decode('UTF-8')
-    print(headers)
+    headers = await read_headers(reader)
     if not 'RTSP/1.0 200 OK' in headers:
         return
     # m2
-    headers = []
-    bytes = await reader.readline()
-    line = bytes.decode('UTF-8')
-    while line != '\r\n':
-        headers.append(line.rsplit('\r\n')[0])
-        bytes = await reader.readline()
-        line = bytes.decode('UTF-8')
-    print(headers)
+    headers = await read_headers(reader)
     if not 'OPTIONS * RTSP/1.0' in headers:
         return
     m2_resp = b'RTSP/1.0 200 OK\r\n\r\n'
@@ -35,6 +33,7 @@ async def connected(reader, writer):
     print(m2_resp)
     await writer.drain()
 
+    # finished
     writer.close()
 
 
